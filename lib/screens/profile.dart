45b881f59fee5a79515d3e38d2e7108827c3ecd9
thembu <rakhivhanithembu@gmail.com>
2024-02-06 16:48:49 +0200

    added user email to profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
@@ -8,10 +9,39 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+
+  final currentUser = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(title: Text('profile page'),),
+      appBar: AppBar(title: Text('profile page'), leading: IconButton(icon :Icon(Icons.arrow_back_ios), onPressed: () => Navigator.pushReplacementNamed(context, '/menu'),),),
+
+
+      body: ListView(
+        children: [
+
+          const SizedBox(height: 50,),
+
+          //profile
+
+
+          Icon(Icons.person , size: 72, color: Colors.blue,),
+
+          // user email
+
+          Text('${currentUser?.email}', textAlign: TextAlign.center,)
+
+
+
+          // name
+
+          //  current location
+
+          //
+        ],
+      ),
+
     );
   }
 }
