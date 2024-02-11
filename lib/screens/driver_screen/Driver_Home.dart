3bf9688ab865b9530a5fb52b4f1b66fca2a690fe
thembu <rakhivhanithembu@gmail.com>
2024-02-11 04:42:53 +0200

    added time picker ui

--- lib/screens/driver_screen/Driver_Home.dart
+++ lib/screens/driver_screen/Driver_Home.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 import 'package:location/location.dart' ;
 import 'package:provider/provider.dart';
 import 'package:wits_bus/screens/student_screen/drawer.dart';
--- lib/screens/student_screen/timer.dart
+++ lib/screens/student_screen/timer.dart
@@ -8,10 +8,36 @@ class Timer extends StatefulWidget {
 }
 
 class _TimerState extends State<Timer> {
+
+  TimeOfDay? time = const TimeOfDay(hour: 12,minute: 12);
+  List<dynamic> times = [];
+
+
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(title: Text('Timer page'), leading: IconButton(icon :Icon(Icons.arrow_back_ios), onPressed: () => Navigator.pushReplacementNamed(context, '/menu'),),),
+      body: Center(
+        child: Text(
+          '${time!.hour.toString()} : ${time!.minute.toString()}',
+              style: const TextStyle(fontSize: 60),
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async{
+          TimeOfDay? newTime = await showTimePicker(context: context, initialTime: time!);
+          if(newTime != null) {
+            setState(() {
+              time = newTime;
+              times.add(time);
+            });
+          }
+        },
+        child: Icon(Icons.access_time),
+      ),
+
     );
   }
 }
